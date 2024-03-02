98f78338f1bcd1c39a859e55ac22fe93087ac22d
Saif Hashish <saifhashish364@gmail.com>
2024-03-03 01:38:27 +0200

    first commit

---
+++ Users.cpp
@@ -0,0 +1,6 @@
+#include "Users.h"
+
+extern QString usernames[100] = {"admin", "test", "user1", "user2"};
+extern QString passwords[100] = {"admin", "1234", "1101", "1102"};
+extern int ages[100] = {20,21,22,23} ;
+extern int usersCount = 4;
